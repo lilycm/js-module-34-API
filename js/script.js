abbf26417b951ffcbdfc501c742236ccf0af38b3
lilycm <lilychakma733@gmail.com>
2024-02-27 20:14:36 +0600

    added card container

--- js/script.js
+++ js/script.js
@@ -1,8 +1,37 @@
-const phoneData = async() =>{
+const phoneData = async () => {
     const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
     const data = await res.json();
     const phones = data.data
     console.log(phones);
+    displayPhones(phones);
+
+}
+
+const displayPhones = phones => {
+    //step-one 
+    const phoneContainer = document.getElementById('phone-container');
+    phones.forEach(phone => {
+        console.log(phone)
+
+        //step-two create a div
+        const phoneCard = document.createElement('div');
+
+        //step-three set innerHTML
+        phoneCard.classList = `card w-96 bg-base-100 shadow-xl`;
+        phoneCard.innerHTML = `
+            <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" />
+            </figure>
+            <div class="card-body">
+                <h2 class="card-title">Shoes!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div class="card-actions justify-end">
+                    <button class="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
+    `
+    //step-four append
+    phoneContainer.appendChild(phoneCard);
+    });
 
 }
 
